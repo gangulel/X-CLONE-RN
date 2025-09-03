01c4e2bda298bdc9d7ad7b8c1f7855ca35ff15bc
Dishma Gangulel <madushankaaldg.22@uom.lk>
2025-09-03 15:58:46 +0530

    Implement getUserProfile function and add protectRoute middleware for user profile updates

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -4,5 +4,8 @@ import { getUserProfile } from "../controllers/user.controller.js";
 const router = express.Router()
 
 router.get("/profile/:username",getUserProfile);
+router.put("/profile",protectRoute,updateProfile);
+
+// update profile => auth
 
 export default router;
